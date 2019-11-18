f1a49407393de91a51e5ae321ba94576ed50eb2c
Sendeyo <maxmuthomi@gmail.com>
2019-11-18 06:17:42 +0300

    removed mail import

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, url_for, redirect, session
 import os
 from passlib.hash import sha256_crypt
 import data
-from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -10,39 +9,10 @@ app.secret_key = os.urandom(24)
 
 app.config.update(
     DEBUG=True,
-    # EMAIL SETTINGS
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=465,
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME='anonymaxdashone@gmail.com',
-    MAIL_PASSWORD='17571757'
 )
-mail = Mail(app)
-
-buses = [""]
 
 
-def send_mail(email, subject, message):
-    try:
-        msg = Message("My Test Emails!",
-                      sender="anonymaxdashone@gmail.com",
-                      recipients=[email])
-        msg.body = message
-        mail.send(msg)
-        return 'Mail sent!'
-    except Exception as e:
-        return(str(e))
 
-
-@app.route("/emailSender", methods=["GET", "POST"])
-def EmailSender():
-    if request.method == "POST":
-        to = request.form["recipient"]
-        subject = request.form["subject"]
-        email = request.form["email"]
-        # em = "{}  {}  {}".format(to, subject, email)
-        send_mail(to, subject, email)
-    return render_template("/editor.html")
 
 
 @app.route("/home/<string:tile>", methods=["GET", "POST"])
